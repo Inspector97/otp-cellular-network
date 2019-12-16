253930363e32879704ce73dd8dc73fb6a2cb2a6f
khoben <extless@gmail.com>
2019-12-16 12:41:37 +0700

    base_station: Регистрация клиента; Отправка СМС

--- erlang-network/src/base_station.erl
+++ erlang-network/src/base_station.erl
@@ -10,21 +10,62 @@
 -author("artem").
 -behaviour(gen_server).
 
--export([start_link/0, stop/0]).
+-export([start_link/0, stop/0, sms/3, register/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
   terminate/2, code_change/3]).
 
--record(state, {}).
+
+%%-record(state, {}).
 
 %%TODO Несколько станций
 start_link() ->
-  gen_server:start_link({global, baseStation}, ?MODULE, [], []).
+  gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
-  gen_server:cast({global, baseStation}, stop).
+  gen_server:cast({global, ?MODULE}, stop).
+
+register(Pid, Number) ->
+  gen_server:call({global, ?MODULE} , {register, {Pid, Number}}).
+
+sms(From, To, Message) ->
+  gen_server:call({global, ?MODULE}, {sms, {From, To, Message}}).
 
 init([]) ->
-  {ok, #state{}}.
+  {ok, []}.
+
+handle_call({sms, {From, To, Message}}, _From, State) ->
+  io:format("SMS: from: ~p to: ~p data: ~p~n", [From, To, Message]),
+
+%%  looking for 'To' number in register state
+  RetList = lists:foldl(
+    fun({Pid,NameTo}, TempList) ->
+      if (To =:= NameTo) -> [Pid | TempList];
+        true -> TempList
+      end
+    end,
+    [], State),
+
+%%  send for specified name and pid
+  lists:foreach(
+    fun(P) ->
+      P ! {oksms, {oksms, {From, To, Message}}}
+    end,
+    RetList),
+
+  {reply, {oksms, {From, To, Message}}, State};
+
+handle_call({register, {Pid, Number}}, _From, State) ->
+  io:format("Client ~p connecting with pid:~p~n", [Pid, Number]),
+  RetList = lists:foldl(
+    fun({P,NameFrom}, TempList) ->
+      if (Number =:= NameFrom) -> TempList;
+        true -> [{P,NameFrom} | TempList]
+      end
+    end,
+    [], State),
+  NewClientList = [{Pid, Number} | RetList],
+
+  {reply, okregister, NewClientList};
 
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
@@ -34,9 +75,6 @@ handle_cast(stop, State) ->
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
-handle_info({sms, {Number, Message}}, State) ->
-  io:format("SMS for Number ~p '~p'~n", [Number, Message]),
-  {noreply, State};
 handle_info(_Msg, State) ->
   {noreply, State}.
 
